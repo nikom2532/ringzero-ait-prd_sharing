f574e1b9ccf6abf0f082139ac3989e0f37aee9ca
Arming Huang <arming@buildthedot.com>
2014-05-15 18:20:07 +0700

    .

--- Sites/prd_sharing/application/models/home_model.php
+++ Sites/prd_sharing/application/models/home_model.php
@@ -1,36 +1,14 @@
 <?php
-class News_model extends CI_Model {
+class Home_model extends CI_Model {
 
 	public function __construct()
 	{
-		$a =	$this->load->database();
-		var_dump($a);
+		parent::__construct();
+		$this->load->database();
 	}
 	
-	public function get_data($slug = FALSE)
+	public function get_gove()
 	{
-		if ($slug === FALSE)
-		{
-			$query = $this->db->get('news');
-			return $query->result_array();
-		}
-
-		$query = $this->db->get_where('news', array('slug' => $slug));
-		return $query->row_array();
-	}
-	
-	public function set_data()
-	{
-		$this->load->helper('url');
-
-		$slug = url_title($this->input->post('title'), 'dash', TRUE);
-
-		$data = array(
-			'title' => $this->input->post('title'),
-			'slug' => $slug,
-			'text' => $this->input->post('text')
-		);
-
-		return $this->db->insert('news', $data);
+		return $this->db->get('test')->row();
 	}
 }
--- Sites/prd_sharing/application/models/test_model.php
+++ Sites/prd_sharing/application/models/test_model.php
@@ -1,14 +1,36 @@
 <?php
-class Test_model extends CI_Model {
+class test_model extends CI_Model {
 
 	public function __construct()
 	{
-		parent::__construct();
-		$this->load->database();
+		$a =	$this->load->database();
+		var_dump($a);
 	}
 	
-	public function get_test()
+	public function get_data($slug = FALSE)
 	{
-		return $this->db->get('test')->row();
+		if ($slug === FALSE)
+		{
+			$query = $this->db->get('news');
+			return $query->result_array();
+		}
+
+		$query = $this->db->get_where('news', array('slug' => $slug));
+		return $query->row_array();
+	}
+	
+	public function set_data()
+	{
+		$this->load->helper('url');
+
+		$slug = url_title($this->input->post('title'), 'dash', TRUE);
+
+		$data = array(
+			'title' => $this->input->post('title'),
+			'slug' => $slug,
+			'text' => $this->input->post('text')
+		);
+
+		return $this->db->insert('news', $data);
 	}
 }
