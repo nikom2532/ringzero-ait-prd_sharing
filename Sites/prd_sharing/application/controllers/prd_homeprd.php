407b6581990210a91ba6482b8786e003e3522094
Arming Huang <arming@buildthedot.com>
2014-05-15 13:57:36 +0700

    test connect db

--- Sites/prd_sharing/application/controllers/prd_homeprd.php
+++ Sites/prd_sharing/application/controllers/prd_homeprd.php
@@ -4,12 +4,11 @@ class PRD_HomePRD extends CI_Controller {
 	public function __construct()
 	{
 		parent::__construct();
-		$this->load->model('test_model');
+		$this->load->model('Test_model');
 	}
 
 	public function index()
 	{
-		
 		
 // $serverName = "localhost";
 // $connectionInfo = array( "Database"=>"ringzero_ait_prd_sharing", "UID"=>"nikom2532", "PWD"=>"cominter");
@@ -17,10 +16,14 @@ class PRD_HomePRD extends CI_Controller {
 // if( $conn ) {
 // echo "Connection established.<br />";
 // 
-// $query = "SElect * from 'test'";
-// $a = sqlsrv_query($query);
-// echo $a;
-// 
+// $query = "SELECT TOP 1000 [field1]
+      // ,[field2]
+  // FROM [ringzero_ait_prd_sharing].[dbo].[test]";
+// $stmt = sqlsrv_query($conn, $query);
+// // print_r($stmt);
+// while( $row = sqlsrv_fetch_array( $stmt, SQLSRV_FETCH_ASSOC) ) {
+      // echo $row['field1'].", ".$row['field2']."<br />";
+// }
 // }else{
 // echo "Connection could not be established.<br />";
 // echo "<pre>";
@@ -29,10 +32,11 @@ class PRD_HomePRD extends CI_Controller {
 // }
 		
 		
-		
 		$data['title'] = 'Home PRD';
 		
 		// $data['test'] = $this->test_model->get_test();
+		
+		// echo $data['test'];
 // 		
 		// var_dump($data['test']);
 
