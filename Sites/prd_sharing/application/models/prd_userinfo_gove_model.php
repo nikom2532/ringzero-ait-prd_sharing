9eb78ba6f731ecee1b16fd8a43407eda99bb2d49
Arming Huang <arming@buildthedot.com>
2014-06-11 10:12:00 +0700

    user info gove

--- Sites/prd_sharing/application/models/prd_userinfo_gove_model.php
+++ Sites/prd_sharing/application/models/prd_userinfo_gove_model.php
@@ -62,6 +62,8 @@ class PRD_UserInfo_GOVE_model extends CI_Model {
 			'Mem_Status' => $mem_status
 		);
 		
+		var_dump($data);
+		
 		$query_setMember = $this->db->
 			where('Member.Mem_ID', $Mem_ID)->
 			update('Member', $data);
