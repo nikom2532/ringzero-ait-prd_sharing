0dde8a9a84b454c4e3f4bc043b311b96462ae65e
Arming Huang <arming@buildthedot.com>
2014-07-07 14:58:01 +0700

    Database: query added

---
+++ Database/Query/Server_prdsharing_Member_create_GOVE_User.sql
@@ -0,0 +1,67 @@
+INSERT INTO [NNT_PRD_Sharing].[dbo].[Member]
+           ([Mem_Name]
+           ,[Mem_LasName]
+           ,[Mem_EngName]
+           ,[Mem_EngLasName]
+          
+           ,[Mem_Password]
+           ,[Mem_Username]
+           ,[Mem_Status]
+           ,[Group_ID]
+           )
+     VALUES
+           (
+           'goverment1',
+           'goverment1',
+           'goverment1',
+           'goverment1',
+           'go1ve',
+           'goverment1',
+           1,
+           1
+           )
+INSERT INTO [NNT_PRD_Sharing].[dbo].[Member]
+           ([Mem_Name]
+           ,[Mem_LasName]
+           ,[Mem_EngName]
+           ,[Mem_EngLasName]
+          
+           ,[Mem_Password]
+           ,[Mem_Username]
+           ,[Mem_Status]
+           ,[Group_ID]
+           )
+     VALUES
+           (
+           'goverment1',
+           'goverment1',
+           'goverment1',
+           'goverment1',
+           'gove2',
+           'goverment1',
+           1,
+           1
+           )
+INSERT INTO [NNT_PRD_Sharing].[dbo].[Member]
+           ([Mem_Name]
+           ,[Mem_LasName]
+           ,[Mem_EngName]
+           ,[Mem_EngLasName]
+          
+           ,[Mem_Password]
+           ,[Mem_Username]
+           ,[Mem_Status]
+           ,[Group_ID]
+           )
+     VALUES
+           (
+           'goverment1',
+           'goverment1',
+           'goverment1',
+           'goverment1',
+           '3gove',
+           'goverment1',
+           1,
+           1
+           )
+GO
---
+++ Database/Query/prdsharing_Member_create_GOVE_User.sql
@@ -0,0 +1,23 @@
+INSERT INTO [ringzero_ait_prd_sharing].[dbo].[Member]
+           ([Mem_Name]
+           ,[Mem_LasName]
+           ,[Mem_EngName]
+           ,[Mem_EngLasName]
+          
+           ,[Mem_Password]
+           ,[Mem_Username]
+           ,[Mem_Status]
+           ,[Group_ID]
+           )
+     VALUES
+           (
+           'goverment3',
+           'goverment3',
+           'goverment3',
+           'goverment3',
+           'gove3',
+           'goverment3',
+           1,
+           1
+           )
+GO
